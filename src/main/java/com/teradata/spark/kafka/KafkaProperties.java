2b99d0f5b999cd8b0e95af400d6b8df96e1babff
Ccccong <caiCongcong0322>
2018-06-27 16:06:33 +0800

    Initial commit

---
+++ src/main/java/com/teradata/spark/kafka/KafkaProperties.java
@@ -0,0 +1,9 @@
+package com.teradata.spark.kafka;
+
+/**@author CCC
+ * kafka常用配置文件
+ */
+
+public class KafkaProperties {
+
+}
---
+++ src/main/scala/com/teradata/spark/App.scala
@@ -0,0 +1,9 @@
+package com.teradata.spark
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
