57f9a525a573dae048832b0105e8a1d4d5cbf35f
Ccccong <caiCongcong0322>
2018-07-14 13:03:02 +0800

    更新代码

---
+++ src/main/java/com/teradata/spark/kafka/KafkaProducer.java
@@ -0,0 +1,25 @@
+package com.teradata.spark.kafka;
+
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.ProducerConfig;
+
+import java.util.Properties;
+
+/**
+ * @author ccc
+ * kafka producer properties
+ */
+public class KafkaProducer {
+    private  String topic;
+    private Producer<String,Integer> producer;
+
+    public KafkaProducer(String topic) {
+        this.topic = topic;
+        Properties properties=new Properties();
+        properties.put("metadata.broker.list",KafkaProperties.BROKER_lIST);
+
+        producer=new Producer<String, Integer>(new ProducerConfig(properties));
+
+    }
+}
