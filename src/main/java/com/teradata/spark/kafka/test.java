1afe8943112d64647fdbb1978b88bd134c9c17cd
Ccccong <caiCongcong0322>
2018-07-21 16:42:46 +0800

    测试

--- src/main/java/com/teradata/spark/kafka/test.java
+++ src/main/java/com/teradata/spark/kafka/test.java
@@ -3,7 +3,7 @@ package com.teradata.spark.kafka;
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("hello world!");
+        System.out.println("hello world and ccc xy");
 
     }
 }
