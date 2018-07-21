588a03d80e2625491ee49de24ace867fcb384ef8
Ccccong <caiCongcong0322>
2018-07-21 10:36:50 +0800

    更新代码

--- src/main/java/com/teradata/spark/kafka/KafkaProducer.java
+++ src/main/java/com/teradata/spark/kafka/KafkaProducer.java
@@ -22,4 +22,8 @@ public class KafkaProducer {
         producer=new Producer<String, Integer>(new ProducerConfig(properties));
 
     }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
